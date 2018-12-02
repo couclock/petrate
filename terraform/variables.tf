14bba53090d63f22e35e0eef664f98067882aa2f
Dany Lecoq <danylecoq@free.fr>
2018-12-02 10:14:03 +0100

    Explain how to get one key

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,6 @@
 # Add a file secret.tf containing with valid access_key
+# Go AWS console > IAM > Users > Pick a user > Security credentials > Create access key
+# to get one valid
 #provider "aws" {
 #  access_key = "AKIAJYB7CZUWZJOATOQQ"
 #  secret_key = "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"
