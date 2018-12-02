6578f0e8bc057c2cb89427097325cadb98a3e9fb
Dany Lecoq <danylecoq@free.fr>
2018-12-02 10:09:48 +0100

    Remove secret

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  access_key = "AKIAJYB7CZUWZJOATOQQ"
-  secret_key = "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"
-  region     = "eu-west-1"
-}
-
+# Add a file secret.tf containing with valid access_key
+#provider "aws" {
+#  access_key = "AKIAJYB7CZUWZJOATOQQ"
+#  secret_key = "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"
+#  region     = "eu-west-1"
+#}
 
 variable "cluster-name" {
   default = "terraform-eks-demo"
